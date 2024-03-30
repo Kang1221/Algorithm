5b6071c90ba4e06e1dc33c0a3fcfb8ceac370e22
Yeonsoo Kang <90603399+Kang1221@users.noreply.github.com>
2024-03-30 23:15:41 +0900

    Create boj10808.cpp

---
+++ week1/boj10808.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[26];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  string s;
+  cin >> s;
+  
+  for(auto c : s)
+    arr[c-'a']++;
+  
+  for(int i = 0; i < 26; i++)
+    cout << arr[i] << ' ';
+}
