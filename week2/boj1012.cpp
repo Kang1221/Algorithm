2c207e924ded98be12510f5bfe5b43182672d64d
Yeonsoo Kang <90603399+Kang1221@users.noreply.github.com>
2024-04-03 23:26:37 +0900

    feat: create boj1012.cpp

---
+++ week2/boj1012.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+int board[51][51];
+bool vis[51][51];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int m, n, k;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  return 0;
+}
