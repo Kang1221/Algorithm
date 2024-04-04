224d2ab6e067f969e272b6c9e4fdddaa0e492298
Yeonsoo Kang <90603399+Kang1221@users.noreply.github.com>
2024-04-04 23:34:11 +0900

    feat: implement bfs

--- week2/boj1012.cpp
+++ week2/boj1012.cpp
@@ -10,6 +10,22 @@ int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
 int m, n, k;
 
+void bfs(int x, int y) {
+  vis[x][y] = true;
+  q.push({ x,y });
+  while (!q.empty()) {
+    auto cur = q.front(); q.pop();
+    for (int dir = 0; dir < 4; dir++) {
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if (vis[nx][ny] || board[nx][ny] != 1) continue;
+      vis[nx][ny] = true;
+      q.push({ nx,ny });
+    }
+  }
+}
+
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
