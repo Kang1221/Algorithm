4ec367f9b3edbff0f57a2875f5de236fc3d20b4d
Yeonsoo Kang <90603399+Kang1221@users.noreply.github.com>
2024-04-07 22:07:48 +0900

    feat: complete boj1012.cpp

--- week2/boj1012.cpp
+++ week2/boj1012.cpp
@@ -30,5 +30,35 @@ int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  //input
+  int t;
+  cin >> t;
+  
+  while (t--) {
+    cin >> m >> n >> k;
+    int x, y;
+    for (int i = 0; i < k; i++) {
+      cin >> x >> y;
+      board[y][x] = 1;
+    }
+
+    //calculate
+    int res = 0; //지렁이 개수
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (board[i][j] == 1 && !vis[i][j]) {
+          bfs(i, j);
+          res++;
+        }
+      }
+    }
+
+    //output
+    cout << res << "\n";
+    for(int i = 0; i < n; i++){
+      fill(board[i], board[i]+m, 0);
+      fill(vis[i], vis[i]+m, false);
+    }
+  }
   return 0;
 }
