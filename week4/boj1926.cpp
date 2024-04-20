719f6497120deeee4270fe7ca73bd0345f8e9b39
Yeonsoo Kang <90603399+Kang1221@users.noreply.github.com>
2024-04-20 21:42:55 +0900

    chore: add comment

--- week4/boj1926.cpp
+++ week4/boj1926.cpp
@@ -32,18 +32,20 @@ int main(){
       while(!Q.empty()){
         area++; // 큐에 들어있는 원소를 하나 뺄 때 마다 넓이를 1 증가시킴
         pair<int,int> cur = Q.front(); Q.pop();
-        for(int dir = 0; dir < 4; dir++){ // 상하좌우 칸을 살펴볼 것이다.
+        for(int dir = 0; dir < 4; dir++){ // 상하좌우 
           int nx = cur.X + dx[dir];
-          int ny = cur.Y + dy[dir]; // nx, ny에 dir에서 정한 방향의 인접한 칸의 좌표가 들어감
-          if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue; // 범위 밖일 경우 넘어감
+          int ny = cur.Y + dy[dir]; // nx, ny에 dir에서 정한 방향의 인접한 칸의 좌표
+          if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue; // 범위 밖일  
+          
           if(vis[nx][ny] || board[nx][ny] != 1) continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
           vis[nx][ny] = 1; // (nx, ny)를 방문했다고 명시
           Q.push({nx,ny});
         }
       }
-      // (i, j)를 시작점으로 하는 BFS가 종료됨
-      mx = max(mx, area); // area가 mx보다 클 경우 mx에 area를 대입. max는 STL에 정의된 함수
+      //bfs 끗
+      mx = max(mx, area); // area가 mx보다 클 경우 mx에 area를 대입
     }
   }
+  //output
   cout << num << '\n' << mx;
 }
